7bfbece8cdd4a3d222d0ce8c3989a623b660b609
Amr Mohamad <amrmosleh@yahoo.com>
2023-05-06 15:36:51 -0400

    .

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-aetInterval(setClock, 1000)
+function displayTime() {
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
 
-function setClock() {
-    const currentDate = new Date()
-    const secondsRatio = currentDate.getMinutes() / 60
-    const minutesDate = (secondsRatio + currentDate.getSeconds()) / 60
-    const hoursRatio = (minutesDate + currentDate.getHours()) / 12
+    // adding if rule to specify day and night time
+    if(hrs >=12){
+        session.innerHTML ='PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    if(hrs > 12){
+        hrs = hrs -12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
 }
+setInterval(displayTime, 10);
